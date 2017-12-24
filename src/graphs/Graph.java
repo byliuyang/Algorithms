b00da253bcb0a389ea19d1b9cea8688dddd0f859
Yang Liu <byliuyang11@gmail.com>
2017-12-24 03:04:33 -0500

    Add Graph with array of adjacency lists

---
+++ src/graphs/Graph.java
@@ -0,0 +1,70 @@
+package graphs;
+
+import fundamentals.bagQueueStack.bag.Bag;
+import libs.In;
+import libs.StdOut;
+
+public class Graph {
+    private int E;
+    private int V;
+    private Bag<Integer>[] adj;
+    private static final String NEWLINE = System.getProperty("line.separator");
+
+    public Graph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++)
+            adj[v] = new Bag<>();
+    }
+
+    public Graph(In in) {
+        this(in.readInt());
+        int E = in.readInt();
+
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            addEdge(v, w);
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(V + " vertices, " + E + " edges" + NEWLINE);
+
+        for (int v = 0; v < V; v++) {
+            s.append(v + ":");
+            for (int w : adj[v]) {
+                s.append(w + " ");
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Graph G = new Graph(in);
+        StdOut.println(G);
+    }
+}
