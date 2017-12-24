bae9ba8afa175682fc4186e8f688626343801d15
Yang Liu <byliuyang11@gmail.com>
2017-12-24 14:23:54 -0500

    Add GraphClient

---
+++ src/graphs/GraphClient.java
@@ -0,0 +1,41 @@
+package graphs;
+
+import libs.In;
+import libs.StdOut;
+
+public class GraphClient {
+    public static int degree(Graph G, int v) {
+        int degree = 0;
+        for (int w : G.adj(v)) degree++;
+        return degree;
+    }
+
+    public static int maxDegree(Graph G) {
+        int maxDegree = 0;
+        for (int v = 0; v < G.V(); v++)
+            maxDegree = Math.max(maxDegree, degree(G, v));
+        return maxDegree;
+    }
+
+    public static int avgDegree(Graph G) {
+        return 2 * G.E() / G.V();
+    }
+
+    public static int numberOfSelfLoops(Graph G) {
+        int count = 0;
+        for (int v = 0; v < G.V(); v++)
+            for (int w : G.adj(v))
+                if (v == w) count++;
+        return count / 2;
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Graph G = new Graph(in);
+        StdOut.println(G);
+
+        StdOut.println("vertex of maximum degree = " + maxDegree(G));
+        StdOut.println("average degree           = " + avgDegree(G));
+        StdOut.println("number of self loops     = " + numberOfSelfLoops(G));
+    }
+}
