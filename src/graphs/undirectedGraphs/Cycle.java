a7616d31089f20a88fe19d215cb8ce4eee6871eb
Yang Liu <byliuyang11@gmail.com>
2017-12-25 14:29:06 -0500

    Add cycle detection

---
+++ src/graphs/undirectedGraphs/Cycle.java
@@ -0,0 +1,40 @@
+package graphs.undirectedGraphs;
+
+import libs.In;
+import libs.StdOut;
+
+/**
+ * Cycle detection. Support this query: Is a given graph acylic?
+ * Assumes no self-loops or parallel edges
+ */
+public class Cycle {
+    private boolean hasCycle;
+    private boolean[] marked;
+
+    public Cycle(Graph G) {
+        marked = new boolean[G.V()];
+        for(int s = 0; s < G.V(); s++)
+            if(!marked[s]) dfs(G, s, s);
+    }
+
+    private void dfs(Graph G, int v, int u) {
+        marked[v] = true;
+        for(int w: G.adj(v))
+            if(!marked[w]) dfs(G, w, v);
+            else if(w != u) hasCycle = true;
+    }
+
+    public boolean hasCycle() {
+        return hasCycle;
+    }
+
+    public static void main(String[] args) {
+        Graph G = new Graph(new In(args[0]));
+        Cycle cycle = new Cycle(G);
+
+        if(cycle.hasCycle()) StdOut.print("HAS");
+        else StdOut.print("NO");
+
+        StdOut.println(" cycle");
+    }
+}
