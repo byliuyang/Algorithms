f05959d0d0f651c5d92b72ee45321c628749920e
Yang Liu <byliuyang11@gmail.com>
2017-12-28 21:41:21 -0500

    Add DepthFirstDirectedPath

---
+++ src/graphs/directedGraphs/DepthFirstDirectedPath.java
@@ -0,0 +1,58 @@
+package graphs.directedGraphs;
+
+import fundamentals.bagQueueStack.stack.Stack;
+import libs.In;
+import libs.StdOut;
+
+public class DepthFirstDirectedPath {
+    private boolean marked[];
+    private int source;
+    private int[] parent;
+
+    DepthFirstDirectedPath(Digraph digraph, int source) {
+        marked = new boolean[digraph.V()];
+        parent = new int[digraph.V()];
+        this.source = source;
+
+        dfs(digraph, source);
+    }
+
+    private void dfs(Digraph digraph, int vertex) {
+        marked[vertex] = true;
+        for (int adjacent : digraph.adj(vertex))
+            if (!marked[adjacent]) {
+                parent[adjacent] = vertex;
+                dfs(digraph, adjacent);
+            }
+    }
+
+    public boolean hasPathTo(int vertex) {
+        return marked[vertex];
+    }
+
+    public Iterable<Integer> pathTo(int vertex) {
+        if (!hasPathTo(vertex)) return null;
+
+        Stack<Integer> path = new Stack<>();
+        for (int x = vertex; x != source; x = parent[x])
+            path.push(x);
+        path.push(source);
+
+        return path;
+    }
+
+    public static void main(String[] args) {
+        Digraph digraph = new Digraph(new In(args[0]));
+        int s = Integer.parseInt(args[1]);
+        DepthFirstDirectedPath paths = new DepthFirstDirectedPath(digraph, s);
+
+        for (int v = 0; v < digraph.V(); v++) {
+            StdOut.print(s + " to " + v + ": ");
+            if (paths.hasPathTo(v))
+                for (int x : paths.pathTo(v))
+                    if (x == s) StdOut.print(x);
+                    else StdOut.print("-" + x);
+            StdOut.println();
+        }
+    }
+}
